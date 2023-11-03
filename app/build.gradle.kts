35424b16e593b95c724773dbc37c6cfcabbae9ce
MoatazBadawy <moataz.egy101@gmail.com>
2023-11-03 11:37:16 +0200

    feat: change app color theme. downgrade minsdk from 24 to 21 android api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.moataz.fetchtask"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/moataz/fetchtask/app/main/MainActivity.kt
+++ app/src/main/java/com/moataz/fetchtask/app/main/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.moataz.fetchtask.app.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.moataz.fetchtask.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        } else {
+            window.statusBarColor = resources.getColor(R.color.main_color)
+        }
     }
 }
